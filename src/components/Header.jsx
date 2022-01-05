737d19acd430670810f95880e13f2e749d88ae18
poohcom1 <pooh677@gmail.com>
2022-01-05 21:32:19 +0700

    Disabled warning

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,7 @@ export default class Header extends React.Component {
      * 
      * @param {function} props.setUser
      */
+    // eslint-disable-next-line no-useless-constructor
     constructor(props) {
         super(props)
 
